d584c40a68e0293e265315bc0fceff10a15e2a2a
CristianVieira27 <breezxyvieira@gmail.com>
2025-05-12 03:55:30 -0300

    Estrutura básica do código PHP

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Olá Mundo! em PHP</title>
+</head>
+<body>
+    <?php
+    echo "<h1>Olá Mundo!</h1>";
+    echo "<p>Este é o meu primeiro programa em PHP.</p>";
+    ?>
+</body>
+</html>
 
-echo "olá mundo! <br>";
+*Utilizar extensão PHP Awesome Snippets
