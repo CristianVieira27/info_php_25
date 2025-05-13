bc9c241156b80fbfb47cb45557847f6a1d7b1c95
Cristian Vieira <breezxyvieira@gmail.com>
2025-05-13 00:39:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -12,3 +12,12 @@
 </html>
 
 *Utilizar extensão PHP Awesome Snippets
+
+
+Conceitos básicos de PHP (resumido)
+Conceito	Exemplo
+Variável	$nome = "Maria";
+Condicional	if ($idade > 18) { ... }
+Loop	for ($i = 0; $i < 5; $i++)
+Função	function saudacao() { ... }
+Array	$lista = ["a", "b", "c"];
