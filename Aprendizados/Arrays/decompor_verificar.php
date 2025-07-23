125896322b8afdf44121546659f0b1f4e52da5b7
CristianVieira27 <breezxyvieira@gmail.com>
2025-07-22 23:59:34 -0300

    Correções.

---
+++ Aprendizados/Arrays/decompor_verificar.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Decompor um valor informado pelo usuario em reais
+ * informando quantas notas de cada representam esse valor.
+ * $notas = [2, 5, 10, 20, 50, 100, 200];
+ * $valor = 170;
+ *
+ * Saida esperada: 1 nota de 100, 1 nota de 50 e 1 nota de 20.
+ * $totalNotas["100"] += 1;
+ * Não utilizar funcoes prontas do PHP.
+ */
+$totalNotas = [
+    "200" => 0,
+    "100" => 0,
+    "50" => 0,
+    "20" => 0,
+    "10" => 0,
+    "5" => 0,
+    "2" => 0,
+];
+$valor = 1057;
+$notas = [200, 100, 50, 20, 10, 5, 2];
+
+$tamanhoArray = count($notas); // 7
+
+for ($i = 0; $i < $tamanhoArray; $i++) {
+    $nota = $notas[$i];
+
+    if ($valor >= $nota) {
+        $quantidade = (int) ($valor / $nota); // 1.2 => 1
+        $valor -= $nota * $quantidade;
+        $totalNotas[$nota] += $quantidade;
+    }
+
+    if ($valor == 0) {
+        break;
+    }
+}
+
+foreach ($totalNotas as $notas => $quantidade) {
+    // 1 nota de 100;
+    echo "$quantidade nota(s) de $notas. <br>";
+}
--- Aprendizados/Arrays/somar_multip_arrays.php
+++ Aprendizados/Arrays/somar_multip_arrays.php
@@ -1,32 +1,31 @@
 <?php
 //Somar dois arrays
+$soma = [];
 $numerosA = [10, 20, 30, 40, 50];
 $numerosB = [5, 45, 37, 2, 25];
 
-$soma = [];
 for ($i = 0; $i < count($numerosA); $i++) {
 
     $somaIndice = $numerosA[$i] + $numerosB[$i];
     $soma[] = $somaIndice;
 }
 for ($i = 0; $i < count($soma); $i++) {
-
     echo $soma[$i] . "<br>";
 }
 
 echo "<hr>";
 
 //Multiplicar dois arrays
+$multiplicar = [];
 $numerosA = [10, 20, 30, 40, 50];
 $numerosB = [5, 45, 37, 2, 25];
 
-$multiplicar = [];
 for ($i = 0; $i < count($numerosA); $i++) {
 
     $multiplicaIndice = $numerosA[$i] * $numerosB[$i];
     $multiplicar[] = $multiplicaIndice;
-
 }
 for ($i = 0; $i < count($multiplicar); $i++) {
+
     echo $multiplicar[$i] . "<br>";
 }
