e019322941cd80bb6d1204eb85d71ab1bbc9cd64
Cristian <breezxyvieira@gmail.com>
2025-07-22 21:48:54 -0300

    Aprendizados e correções. Arrays e objeto

---
+++ Calculadora.php
@@ -0,0 +1,24 @@
+<?php
+
+//Nome do objeto como definição 
+//Fazer todas as funções 
+class Calculadora {
+ public $resultado = 0;
+    public function somar($valor1, $valor2) {
+    $this->resultado = $valor1 + $valor2;    
+    }
+}
+
+
+public function subtrair($valor1, $valor2) {
+    $this->resultado = $valor1 - $valor2;    
+    }
+    
+
+public function dividir($valor1, $valor2) {
+    $this->resultado = $valor1 - $valor2;    
+    }
+
+public function multiplicar($valor1, $valor2) {
+    $this->resultado = $valor1 - $valor2;    
+    }
