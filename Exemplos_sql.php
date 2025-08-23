6ae03c2c7c550d0135cbc3391c34220f3559c538
Cristian <breezxyvieira@gmail.com>
2025-08-23 00:19:39 -0300

    Adicionando consulta de script

--- Exemplos_sql.php
+++ Exemplos_sql.php
@@ -1,4 +1,3 @@
-
 SELECT u.login, u.nome_usuario AS "nome", p.sexo, e.cidade FROM usuarios AS u
 INNER JOIN pessoas p ON u.id = p.id_usuario
 INNER JOIN endereco e ON p.id = e.id_pessoa;
@@ -12,25 +11,65 @@ SELECT u.login, u.nome_usuario AS "nome", p.sexo FROM usuarios AS u
 RIGHT JOIN pessoas p ON u.id = p.id_usuario;
 
 
---  $retornoBanco = [
---      [
---          "login" => "fulano",
---          "nome" => "fulano de tal",
---          "sexo" => "M",
---      ],
---      [
---          "login" => "joao.caminhao",
---          "nome" => "joao.caminhao",
---          "sexo" => "M",
---      ]
---  ];
+-- $retornoBanco = [
+-- [
+-- "login" => "fulano",
+-- "nome" => "fulano de tal",
+-- "sexo" => "M",
+-- ],
+-- [
+-- "login" => "joao.caminhao",
+-- "nome" => "joao.caminhao",
+-- "sexo" => "M",
+-- ]
+-- ];
 
---  foreach ($retornoBanco as $idx => $arrayAssociativo) {
+-- foreach ($retornoBanco as $idx => $arrayAssociativo) {
 
---      echo $arrayAssociativo["login"];
---      echo $arrayAssociativo["nome"];
---      echo $arrayAssociativo["sexo"];
+-- echo $arrayAssociativo["login"];
+-- echo $arrayAssociativo["nome"];
+-- echo $arrayAssociativo["sexo"];
 
---  }
+-- }
 
---  echo $retornoBanco[1]["login"]; // "joao.caminhao"
+-- echo $retornoBanco[1]["login"]; // "joao.caminhao"
+
+
+
+
+
+<?php
+/* meu script
+
+ CRUD
+
+SELECT * FROM usuarios u;
+
+SELECT u.login, u.nome_usuario AS "nome", p.sexo, e.cidade FROM usuarios u
+INNER JOIN pessoas p ON u.id = p.id_usuario
+INNER JOIN enderecos e ON p.id = e.id_pessoas;
+
+SELECT u.login, u.nome_usuario AS "nome", p.sexo FROM usuarios AS u
+LEFT JOIN pessoas p ON u.id = p.id_usuario;
+
+SELECT u.login, u.nome_usuario AS "nome", p.sexo FROM usuarios AS u
+RIGHT JOIN pessoas p ON u.id = p.id_usuario;
+
+SELECT p.nome, p.data_de_nascimento, p.sexo, p.estado_civil, p.escolaridade, p.telefone, p.email, p.id_usuario  FROM usuarios u
+RIGHT JOIN pessoas p ON u.id = p.id_usuario;
+
+SELECT u.id, u.login, p.nome, e.logradouro, e.numero, e.cidade FROM usuarios u 
+LEFT JOIN pessoas p ON u.id = p.id_usuario
+LEFT JOIN enderecos e ON p.id = e.id_pessoas;
+
+SELECT p.id_usuario, p.nome, p.data_de_nascimento, p.sexo, p.estado_civil, p.escolaridade, p.telefone, p.email FROM pessoas p;
+
+SELECT p.id_usuario, p.nome, p.data_de_nascimento, p.sexo, p.estado_civil, p.escolaridade, p.telefone, p.email,
+e.bairro, e.logradouro, e.cidade, e.estado, e.cep, e.pais
+FROM pessoas p JOIN enderecos e ON p.id_usuario  = e.id_pessoas;
+
+SELECT * FROM pessoas p;
+
+SELECT * FROM enderecos e;
+*/
+?>
